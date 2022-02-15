d06077e6ba719f4128fbab2b7bc25fb62fea9365
regisfaria <rgisfaria@yahoo.com.br>
2022-02-15 00:14:43 -0300

    moved some files

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
-import { UsersResolver } from '../modules/users/graphql/resolvers/UsersResolver';
-import { PetsResolver } from '../modules/pets/graphql/resolvers/PetsResolver';
+import { UsersResolver } from './modules/users/graphql/resolvers/UsersResolver';
+import { PetsResolver } from './modules/pets/graphql/resolvers/PetsResolver';
 
 async function init() {
   const app = express();
